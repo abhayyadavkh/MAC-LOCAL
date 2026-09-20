6ff85533d9934c74ef42bcf6aec383cf69607bfc
Abhay Yadav <abhayyofficial@gmail.com>
2026-09-21 01:39:39 +0530

    custom addon created
    custom addon
    
    fixed

---
+++ custom/trainingstorefrontaddon/src/com/hybris/training/constants/TrainingstorefrontaddonConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.hybris.training.constants;
+
+/**
+ * Global class for all Trainingstorefrontaddon constants. You can add global constants for your extension into this class.
+ */
+
+public final class TrainingstorefrontaddonConstants extends GeneratedTrainingstorefrontaddonConstants
+{
+	public static final String EXTENSIONNAME = "trainingstorefrontaddon";
+
+	private TrainingstorefrontaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
