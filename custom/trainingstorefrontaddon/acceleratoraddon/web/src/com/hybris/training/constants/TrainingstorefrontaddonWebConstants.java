4c0271f4400fede316e6a7e46c1500f7ce01b59c
Abhay Yadav <abhayyofficial@gmail.com>
2026-09-21 01:39:39 +0530

    custom addon created

---
+++ custom/trainingstorefrontaddon/acceleratoraddon/web/src/com/hybris/training/constants/TrainingstorefrontaddonWebConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.hybris.training.constants;
+
+/**
+ * Global class for all Trainingstorefrontaddon web constants. You can add global constants for your extension into this class.
+ */
+public final class TrainingstorefrontaddonWebConstants 
+{
+	private TrainingstorefrontaddonWebConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
